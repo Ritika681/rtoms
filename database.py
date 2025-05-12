3462df9c7393a6f1d355aed5c1f9c700b07a8f67
Ritika681 <2023mt03177@wilp.bits-pilani.ac.in>
2025-05-13 00:54:00 +0530

    test

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
-
+import os
 
 DB_USERNAME = "root"
 DB_PASSWORD = "root"
@@ -12,6 +12,18 @@ DB_PORT = "3306"
 DB_NAME = "rtoms"
 
 DATABASE_URL = f"mysql+aiomysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+IS_TEST = os.getenv("TEST_ENV", "0") == "1"
+
+DATABASE_URL = (
+    "sqlite+aiosqlite:///./test.db"
+    if IS_TEST
+    else f"mysql+aiomysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+
+
+
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
